7dd005d21ce4f7e83878248c96b0dc5d346d1077
Laís Campos <131169073+laiscampos1@users.noreply.github.com>
2024-09-18 10:44:56 -0300

    Update Webscraping.js

--- Webscraping.js
+++ Webscraping.js
@@ -1,31 +1,7 @@
-async function buscarImagensDosPersonagens(url) {
+buscarImagensDosPersonagens() {
     
-  
-  //Site utilizado
-   let urlAlvo = 'https://sanrio.com.br';
-   let $ = cheerio.load(conteudoHtml);
-  
-      const container = $('.characters-container');
-  
-      if (!container.length) {
-        throw new Error('Elemento container de personagens não encontrado.');
-      }
-  
-      const urlsImagens = container.find('img')
-        .map((_, elemento) => $(elemento).attr('src'))
-        .get();
-  
-      return urlsImagens;
-    } catch (error) {
-      console.error('Erro ao buscar ou analisar imagens:', error.message);
-      return []; 
-    }
-  
-  
-  buscarImagensDosPersonagens(urlAlvo)
-    .then(urlsImagens => {
-      console.log('URLs de imagens extraídas:', urlsImagens);
-    })
-    .catch(error => {
-      console.error('Erro ao buscar imagens:', error.message);
-    });
+ //Site utilizado 'https://sanrio.com.br';
+ let imagens = document.querySelectorAll("div.characters-banner img")
+imagens.forEach(imagem=>{
+    console.log(imagem)
+})
